d775068320a4c07ae4529ff8b1ade44a6bca6b8f
nardm <lenar.thg@gmail.com>
2019-04-15 00:18:21 +0300

    туц

--- src/app/theme/left-bar/meter-reading/meter-reading.component.ts
+++ src/app/theme/left-bar/meter-reading/meter-reading.component.ts
@@ -1,16 +1,50 @@
 import { Component, OnInit } from "@angular/core";
 import { RadSideDrawer } from "nativescript-ui-sidedrawer";
 import * as app from "tns-core-modules/application";
+import { IPropertyAnnotation } from "~/app/theme/home/service/shared/service.model";
 
 @Component({
     selector: "Browse",
     moduleId: module.id,
-    templateUrl: "./meter-reading.component.html"
+    templateUrl: "./meter-reading.component.html",
+    styleUrls: ["./meter-reading.component.scss"]
 })
 export class MeterReadingComponent implements OnInit {
 
+    metadata: any;
+    store: any;
+
     constructor() {
         // Use the component constructor to inject providers.
+
+        this.metadata = {
+            commitMode: "immediate",
+            isReadOnly: false,
+            validationMode: "immediate",
+            propertyAnnotations: [{
+                name: "text1",
+                displayName: "Электроснобжение день",
+                index: 1,
+                editor: "Number"
+            }, {
+                name: "text2",
+                displayName: "Электроснобжение ночь",
+                index: 2,
+                editor: "Number"
+            }, {
+                name: "text3",
+                displayName: "Горячее водоснабжение",
+                index: 3,
+                editor: "Number"
+            }, {
+                name: "text4",
+                displayName: "Холодное водоснабжение",
+                index: 4,
+                editor: "Number"
+            }
+            ]
+        };
+        this.store = {text1: "", text2: "", text3: "", text4: ""};
     }
 
     ngOnInit(): void {
@@ -20,5 +54,9 @@ export class MeterReadingComponent implements OnInit {
     onDrawerButtonTap(): void {
         const sideDrawer = <RadSideDrawer>app.getRootView();
         sideDrawer.showDrawer();
+    }
+
+    onSubmit() {
+
     }
 }
--- src/app/theme/left-bar/meter-reading/meter-reading.module.ts
+++ src/app/theme/left-bar/meter-reading/meter-reading.module.ts
@@ -3,11 +3,15 @@ import { NativeScriptCommonModule } from "nativescript-angular/common";
 
 import { MeterReadingRoutingModule } from "./meter-reading-routing.module";
 import { MeterReadingComponent } from "./meter-reading.component";
+import { NativeScriptUIDataFormModule } from "nativescript-ui-dataform/angular";
+import { NativeScriptFormsModule } from "nativescript-angular";
 
 @NgModule({
     imports: [
         NativeScriptCommonModule,
-        MeterReadingRoutingModule
+        MeterReadingRoutingModule,
+        NativeScriptUIDataFormModule,
+        NativeScriptFormsModule
     ],
     declarations: [
         MeterReadingComponent
